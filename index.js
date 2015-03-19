20bd2e79ac852cc538b0b18ccebc6ecf46d99cca
Peng Zhong <peng@nylira.com>
2015-03-19 17:53:04 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+(function() {
+'use strict'
+
+var TwoDArray = require('./TwoDArray')
+
+function Grid(height, width){
+  this.height = height !== undefined ? height : 10
+  this.width = width !== undefined? width: height
+  this.cells = new TwoDArray(this.height, this.width)
+}
+
+module.exports = Grid
+}())
